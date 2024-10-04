dd1436297529d655fbd9893e1e819b19e42b743c
jacky <jacky.le.goldenowl@gmail.com>
2024-10-04 18:06:52 +0700

    feat: add pundit and pagy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  include Pagy::Frontend
+
   ActionView::Base.default_form_builder = FormBuilders::DaisyFormBuilder
 
   def component(name, *, **, &)
