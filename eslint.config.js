062d214bcdf72bfccb5b41b3caa7ccb4a297aaba
jacky <102570165+jacky-le-goldenowl@users.noreply.github.com>
2024-10-23 13:19:31 +0700

    feat: check git hook (#33)
    * fix: dup rubocop rules
    
    * feat: add commit lint and update lefthook config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
 /** @type {import("@types/eslint").Linter.Config */
 
-import globals from "globals";
-import js from "@eslint/js";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from 'globals';
+import js from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
   js.configs.recommended,
@@ -10,23 +10,23 @@ export default [
   {
     languageOptions: {
       ecmaVersion: 2022,
-      sourceType: "module",
+      sourceType: 'module',
       globals: {
         ...globals.browser,
         ...globals.es2021,
       },
     },
     rules: {
-      "no-unused-vars": [
-        "error",
+      'no-unused-vars': [
+        'error',
         {
-          args: "after-used",
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
         },
       ],
-      "no-var": "error",
-      "prettier/prettier": "error",
+      'no-var': 'error',
+      'prettier/prettier': 'error',
     },
   },
 ];
